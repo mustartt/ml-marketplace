a9284798d96cc27be7518e68297e4eda9e493e8b
Colin Cai <colincai09@gmail.com>
2021-08-20 16:20:49 -0700

    updated init.sql with Dataset/model table

--- migrations/changeset/init.sql
+++ migrations/changeset/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.product
+CREATE TABLE public.model
 (
     model_id      integer     NOT NULL,
 
@@ -22,4 +22,30 @@ CREATE TABLE public.product
     instance_size integer,
 
     CONSTRAINT PK_models PRIMARY KEY (model_id)
+);
+
+CREATE TABLE public.dataset
+(
+    dataset_id      integer     NOT NULL,
+
+    name          varchar(50) NOT NULL,
+
+    type          varchar(10) NOT NULL,
+    category      varchar(50),
+    framework     varchar(50),
+    format        varchar(50),
+
+    publisher     integer     NOT NULL,
+
+    excerpt       text,
+    description   text,
+    create_date   date        NOT NULL,
+    update_date   date        NOT NULL,
+
+    storage_url   text,
+    tags          text,
+    price         decimal,
+    instance_size integer,
+
+    CONSTRAINT PK_models PRIMARY KEY (dataset_id)
 );
