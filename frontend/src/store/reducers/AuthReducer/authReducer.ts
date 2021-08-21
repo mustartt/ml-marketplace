1fc3ee79c7c884ec4b95e16b6257ceccaa2311c5
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-20 22:54:38 -0700

    basic refresh token authorization

---
+++ frontend/src/store/reducers/AuthReducer/authReducer.ts
@@ -0,0 +1,33 @@
+export type AuthActionType = {
+  type: string;
+  payload: string;
+}
+
+export interface AuthState {
+  isAuthenticated: boolean;
+  accessToken: string;
+}
+
+const initialState = {
+  isAuthenticated: false,
+  accessToken: ''
+}
+
+const authReducer = (state: AuthState = initialState, action: AuthActionType) => {
+  switch (action.type) {
+    case "SET_AUTH":
+      return Object.assign({}, state, {
+        isAuthenticated: true,
+        accessToken: action.payload
+      });
+    case "UNSET_AUTH":
+      return Object.assign({}, state, {
+        isAuthenticated: false,
+        accessToken: ''
+      });
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
