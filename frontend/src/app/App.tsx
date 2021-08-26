6c1a11aaa63043d3867e8c3c86a5d28b06f5429b
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-26 15:22:45 -0700

    Model Page Result View

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './index.scss';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,6 +10,8 @@ import LoginComponent from './components/Login/LoginComponent';
 import AppBgComponent from './components/AppBgComponent';
 import RegisterComponent from './components/RegisterComponent';
 import SearchBox from './components/SearchBox/SearchBox';
+import ModelPageView from './view/ModelResult/ModelPageView';
+import ModelResultView from './view/ModelResult/ModelResultView';
 
 const App: React.FC = () => {
 
@@ -22,50 +24,28 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <div>
-        <h1>Application</h1>
-        <pre>{JSON.stringify(authState, null, 2)}</pre>
-        <button
-          type="button"
-          className="text-gray-100 m-3 bg-indigo-500 font-semibold px-5 py-3 rounded-full shadow"
-          onClick={() => {
-          dispatch(AuthActions.getAccessToken());
-        }}>
-          Reauthenticate
-        </button>
-
-        <ul className="flex space-x-5 px-10 py-10 bg-gray-900">
-          <li className="text-gray-100 bg-indigo-500 font-semibold px-5 py-3 rounded-full shadow">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="text-gray-100 bg-indigo-500 font-semibold px-5 py-3 rounded-full shadow">
-            <Link to="/login">Login</Link>
-          </li>
-          <li className="text-gray-100 bg-indigo-500 font-semibold px-5 py-3 rounded-full shadow">
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/login">
-            <AppBgComponent>
-              <LoginComponent/>
-            </AppBgComponent>
-          </Route>
-          <Route path="/register">
-            <AppBgComponent>
-              <RegisterComponent/>
-            </AppBgComponent>
-          </Route>
-          <Route path="/">
-            <SearchBox
-              isImmediate
-              search={(val) => {
+      <Switch>
+        <Route path="/login">
+          <AppBgComponent>
+            <LoginComponent/>
+          </AppBgComponent>
+        </Route>
+        <Route path="/register">
+          <AppBgComponent>
+            <RegisterComponent/>
+          </AppBgComponent>
+        </Route>
+        <Route path="/models">
+          <ModelResultView />
+        </Route>
+        <Route path="/">
+          <SearchBox
+            isImmediate
+            search={(val) => {
               console.log(val);
-            }} />
-          </Route>
-        </Switch>
-      </div>
+            }}/>
+        </Route>
+      </Switch>
     </Router>
 
   );
