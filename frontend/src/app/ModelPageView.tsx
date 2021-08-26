f3dfa60d9a90700810f36a60a27445c5b64ca557
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-25 18:45:10 -0700

    Model List

---
+++ frontend/src/app/ModelPageView.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import ModelResultComponent from './components/Product/ModelResultComponent';
+
+const IMG_URL = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*';
+const DESC = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque facilisis sem suscipit lacus rhoncus, eu lobortis ligula pulvinar. Sed placerat imperdiet lectus, in auctor libero. Nullam ut augue enim.';
+
+const MODEL_LIST = [
+  {
+    productID: '1',
+    name: 's-r152x4',
+    type: 'Image Classification',
+    img: IMG_URL,
+    excerpt: DESC,
+    publisher: {
+      name: 'mustartt',
+      profileUrl: '#',
+    },
+  },
+  {
+    productID: '2',
+    name: 'mobilenet_v2',
+    type: 'Image Classification',
+    img: IMG_URL,
+    excerpt: DESC,
+    publisher: {
+      name: 'mustartt',
+      profileUrl: '#',
+    },
+    updatedAt: new Date(),
+    tags: [
+      {
+        name: 'tag',
+        link: '#'
+      }
+    ]
+  },
+  {
+    productID: '3',
+    name: 'resnet50_v1',
+    type: 'Image Classification',
+    img: IMG_URL,
+    excerpt: DESC,
+    publisher: {
+      name: 'mustartt',
+      profileUrl: '#',
+    },
+  },
+];
+
+const ModelPageView: React.FC = () => {
+
+  const renderModels = () => {
+    return MODEL_LIST.map((val, index) => (
+        <ModelResultComponent
+          key={index}
+          {...val} />
+      ),
+    );
+  };
+
+  return (
+    <div className="w-full md:p-5 grid grid-cols-1 md:grid-cols-2 place-content-center gap-5">
+      {renderModels()}
+    </div>
+  );
+};
+
+export default ModelPageView;
+
--- frontend/src/app/components/Product/TagComponent.tsx
+++ frontend/src/app/components/Product/TagComponent.tsx
@@ -7,7 +7,7 @@ export type TagType = {
 
 const TagComponent: React.FC<TagType> = ({name, link}) => {
   return (
-    <a href={link && '#'}
+    <a href={link || '#'}
        className="inline-block bg-indigo-600 pt-1 pb-1.5 px-2.5 leading-none font-semibold text-gray-100 rounded-full shadow hover:bg-indigo-500 transition duration-150">
       {name}
     </a>
