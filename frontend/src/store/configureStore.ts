a6eb30f17658ca37400de38d2cdd47b7c26d2837
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-28 15:16:18 -0700

    redux dev tools

--- frontend/src/store/configureStore.ts
+++ frontend/src/store/configureStore.ts
@@ -3,8 +3,15 @@ import thunk from 'redux-thunk';
 
 import rootReducer, { RootState } from './reducers/rootReducer';
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const configureStore = (initialState: RootState) => {
-  return compose(
+  return composeEnhancers(
     applyMiddleware(thunk),
   )(createStore)(rootReducer, initialState);
 };
