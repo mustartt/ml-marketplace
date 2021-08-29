de69ebc5589057bb4e5c93537505b84e63f0ce54
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-29 12:36:52 -0700

    [MLMP-16] page view loading placeholder

--- frontend/src/app/view/ModelResult/ModelPageView.tsx
+++ frontend/src/app/view/ModelResult/ModelPageView.tsx
@@ -8,11 +8,10 @@ const IMG_URL = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/do
 
 const ModelPageView: React.FC = () => {
 
-  const models = useSelector((state: RootState) => state.modelState.models);
+  const state = useSelector((state: RootState) => state.modelState);
 
   const renderModels = () => {
-    return models.map((model: ModelType, index: number) => {
-
+    return state.models.map((model: ModelType, index: number) => {
         const publisherInfo = {
           name: model.publisher.username,
           profileUrl: '', /* not used */
@@ -43,12 +42,36 @@ const ModelPageView: React.FC = () => {
     );
   };
 
-  return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 place-content-center gap-5">
-      {renderModels()}
-    </div>
-  );
+  const renderMainContent = () => {
+    if (state.isLoading) {
+      return (
+        <div className="w-full flex justify-center items-center p-5">
+          <div className="flex items-center animate-pulse text-gray-900 tracking-wide font-semibold text-lg">
+            <svg className="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                 viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
+              <path className="opacity-75" fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+            </svg>
+            <span>Loading...</span>
+          </div>
+        </div>
+      );
+    } else if (state.models.length > 0) {
+      return (
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 place-content-center gap-5">
+          {renderModels()}
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-full flex justify-center items-center p-5">
+          <span className="text-gray-900 tracking-wide font-semibold text-lg">No Results...</span>
+        </div>
+      );
+    }
+  };
+
+  return renderMainContent();
 };
-
 export default ModelPageView;
-
