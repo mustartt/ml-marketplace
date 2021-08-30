9c4843b5a98afc1fbbf9248b2838608ff089e419
Henry Jiang <h243jian@uwaterloo.ca>
2021-08-30 13:16:34 -0700

    [MLMP-17] temp fix for node polyfill in webpack build configs

--- frontend/config/webpack.build.js
+++ frontend/config/webpack.build.js
@@ -1,16 +1,24 @@
 // suppress inspect Unresolved type for while file
 // noinspection JSUnresolvedFunction
 
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    plugins: [
-        new webpack.DefinePlugin({
-            USE_LOCAL_OIDC: JSON.stringify(true),
-        })
-    ],
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: false,
+    }),
+    new webpack.DefinePlugin({
+      process: { /* temp fix not sure why process NODE_DEBUG is not working */
+        env: {
+          NODE_DEBUG: false,
+        },
+      },
+    }),
+  ],
 });
