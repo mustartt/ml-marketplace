4a606fd93287c7cea922e7b56563c26fa2d13676
Henry Jiang <h243jian@uwaterloo.ca>
2021-09-01 17:24:51 -0700

    [MLMP-21] update pagination key props

--- frontend/src/app/components/Pagination/Pagination.tsx
+++ frontend/src/app/components/Pagination/Pagination.tsx
@@ -7,54 +7,41 @@ interface PaginationProps {
   onChange: (newPage: number) => void; // callback when number changes
 }
 
-const Pagination: React.FC<PaginationProps> = (props) => {
+const getPageNumbers = (curr: number, total: number): number[] =>
+  Array.from(new Array(5),
+    (_, x) => x + curr - 2).filter(x => 0 < x && x <= total);
+
+const Pagination: React.FC<PaginationProps> = ({curr, totalPage, onChange}) => {
+
+  const pageNumbers = getPageNumbers(curr, totalPage);
 
   const changePage = (num: number) => {
-    if (num < 0 || num >= props.totalPage)
-      return;
-    props.onChange(num);
+    if (num >= 0 && num < totalPage) {
+      onChange(num);
+    }
   };
-
-  let pages = [];
-  // making pages 5 numbers depending on current
-  if (props.totalPage >= 5) {
-    if (props.curr + 1 < 3) {
-      for (let i = 1; i <= 5; i++) {
-        pages.push(i);
-      }
-    } else if (props.curr + 1 > props.totalPage - 3) {
-      for (let i = props.totalPage - 4; i <= props.totalPage; i++) {
-        pages.push(i);
-      }
-    } else {
-      for (let i = props.curr - 1; i < props.curr + 4; i++) {
-        pages.push(i);
-      }
-    }
-  } else {
-    for (let i = 1; i <= props.totalPage; i++) {
-      pages.push(i);
-    }
-  }
 
   return (
     <div
       className="flex flex-row rounded-full w-min px-2 py-1 leading-none bg-gray-100 space-x-2 items-center text-gray-800 font-semibold text-base space-x-1 shadow-lg">
       <span
         className="cursor-pointer hover:bg-indigo-600 hover:text-gray-200 hover:shadow-md transition duration-150 rounded-full p-1"
-        onClick={() => changePage(props.curr - 1)}>
+        onClick={() => changePage(curr - 1)}>
         <ChevronLeftIcon className="w-5 h-5"/>
       </span>
       {
-        pages.map(num => {
-          if (num === props.curr + 1) {
+        pageNumbers.map(num => {
+          if (num === curr + 1) {
             return <span
-              className="cursor-pointer text-gray-200 bg-indigo-600 hover:bg-indigo-500 shadow-md transition duration-150 rounded-full p-1 px-2">{num}</span>;
+              key={num}
+              className="cursor-pointer text-gray-200 bg-indigo-600 hover:bg-indigo-500 shadow-md transition duration-150 rounded-full p-1 px-2">
+              {num}
+            </span>;
           } else {
             return <span
               className="cursor-pointer text-gray-800 hover:bg-indigo-600 hover:text-gray-200 hover:shadow-md transition duration-150 rounded-full p-1 px-2"
               key={num}
-              onClick={() => changePage(num)}
+              onClick={() => changePage(num - 1)}
             >
               {num}
             </span>;
@@ -63,7 +50,7 @@ const Pagination: React.FC<PaginationProps> = (props) => {
       }
       <span
         className="cursor-pointer hover:bg-indigo-600 hover:text-gray-200 hover:shadow-md transition duration-150 rounded-full p-1"
-        onClick={() => changePage(props.curr + 1)}>
+        onClick={() => changePage(curr + 1)}>
         <ChevronRightIcon className="w-5 h-5"/>
       </span>
     </div>
