8197fcac03874ce8c2735a696b69285e50d1f6d6
Colin Cai <cai9@berkeley.edu>
2021-09-03 15:32:30 -0700

    updated paymentController

--- mlmp-api/src/main/java/com/mlmarketplace/mlmp/rest/StripePaymentController.java
+++ mlmp-api/src/main/java/com/mlmarketplace/mlmp/rest/StripePaymentController.java
@@ -9,14 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+
 
 import java.util.List;
 
@@ -24,7 +26,8 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping(path = "/payment")
 public class StripePaymentController {
 
     @Value("${stripe.keys.public}")
