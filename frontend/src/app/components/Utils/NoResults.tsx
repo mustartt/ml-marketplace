b632de7e0556f5023a4182612fea62a1b9ecfc7c
Henry Jiang <h243jian@uwaterloo.ca>
2021-09-05 19:10:45 -0700

    [MLMP-26] load user in user view

---
+++ frontend/src/app/components/Utils/NoResults.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoResults = () => {
+  return (
+    <div className="w-full flex justify-center items-center p-5">
+      <span className="text-gray-900 tracking-wide font-semibold text-lg">No Results...</span>
+    </div>
+  );
+};
+export default NoResults;
--- frontend/src/app/view/UserView/UserProfileView.tsx
+++ frontend/src/app/view/UserView/UserProfileView.tsx
@@ -1,16 +1,69 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../store/reducers/rootReducer';
+import React, { useEffect, useState } from 'react';
+import UserSummary from './ProfileComponent/UserSummary';
+import ModelExcerpt from './ProfileComponent/ModelExcerpt';
+import { mapUserResponseToUser, ModelType, UserType } from '../../../types/ResponseTypes';
+import { useParams } from 'react-router-dom';
+import { UserResponseType } from '../../../actions/user/userActions';
+import axios from 'axios';
+import ApiRoute from '../../../services/ApiRoutesService';
+import LoadingMessage from '../../components/Utils/LoadingMessage';
+import NoResults from '../../components/Utils/NoResults';
+
+interface UserProfileViewParams {
+  username: string;
+}
 
 const UserProfileView: React.FC = () => {
 
-  const userState = useSelector((state: RootState) => state.userState);
+  const {username} = useParams<UserProfileViewParams>();
+
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [user, setUser] = useState<UserType | null>(null);
+  const [models, setModels] = useState<ModelType[] | null>(null);
+
+  useEffect(() => {
+    setIsUserLoading(true);
+    setIsModelLoading(true);
+
+    axios.get<UserResponseType>(ApiRoute.constructProfileUrlWithUsername(username)).then(res => {
+      const user = mapUserResponseToUser(res.data);
+      setUser(user);
+    }).catch(err => {
+      setUser(null);
+    }).finally(() => {
+      setIsUserLoading(false);
+    });
+  }, []);
+
+  const renderModels = () => {
+    if (models?.length) {
+      return models.map((model, index) => (
+        <ModelExcerpt model={model} key={index}/>
+      ));
+    }
+    return <NoResults/>;
+  };
 
   return (
-    <div className="w-90 p-10 rounded-lg bg-gray-200 text-black text-md">
-      <pre>
-        {JSON.stringify(userState, null, 2)}
-      </pre>
+    <div className="bg-gray-100 w-screen h-screen overflow-hidden flex justify-center">
+      <div className="flex-grow max-w-4xl h-full">
+        <div className="p-1 block sm:flex">
+          {
+            !user ? 'loading' :
+              <UserSummary user={user}/>
+          }
+          <div className="mt-4 w-full max-w-2xl">
+            <h1 className="text-center font-semibold text-xl">Creations</h1>
+            <div className="p-2 w-full flex flex-col justify-center items-center space-y-4">
+              {
+                isModelLoading ? <LoadingMessage/> :
+                  renderModels()
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
