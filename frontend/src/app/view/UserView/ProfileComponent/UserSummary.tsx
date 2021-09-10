b5be7c77b5854f83c27e51a80b98aaad509cca07
Henry Jiang <h243jian@uwaterloo.ca>
2021-09-09 20:58:29 -0700

    [MLMP-26] temp loading profile

--- frontend/src/app/view/UserView/ProfileComponent/UserSummary.tsx
+++ frontend/src/app/view/UserView/ProfileComponent/UserSummary.tsx
@@ -21,4 +21,26 @@ const UserSummary: React.FC<UserSummaryProps> = ({user}) => {
   );
 };
 
+const LoadingUserSummary = () => {
+  return (
+    <div className="animate-pulse flex-shrink-0 p-5 flex flex-col items-center">
+      <div
+        className="flex-shrink-0 bg-gray-400 w-48 h-48 rounded-full border-2 border-indigo-600 border-opacity-50 object-cover shadow-md"
+      />
+      <div className="text-center">
+        <h1 className="mt-3 font-semibold text-2xl text-gray-900">
+          <span className="text-gray-400 bg-gray-400">User Loading</span>
+        </h1>
+        <h3 className="mt-1 text-lg text-gray-500">
+          <span className="text-gray-400 bg-gray-400">username</span>
+        </h3>
+        <h3 className="mt-2 font-semibold text-lg text-gray-700">
+          <span className="text-gray-400 bg-gray-400">user email</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
 export default UserSummary;
+export { LoadingUserSummary };
